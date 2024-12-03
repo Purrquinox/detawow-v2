0b3c56ed43cbba5b00cbb197ceae11e2cffccc83
Select <66577268+selectdev@users.noreply.github.com>
2024-12-03 07:07:03 -0600

    Update haerin.ts

--- src/commands/haerin.ts
+++ src/commands/haerin.ts
@@ -6,7 +6,7 @@ export default {
 		.setDescription("yes."),
 	async execute(client, interaction, EmbedBuilder, codeBlock, db) {
 		await interaction.reply({
-			content: "https://media.tenor.com/AhO27JOKpBAAAAPo/haerin-newjeans.gif",
+			content: "https://media.tenor.com/AhO27JOKpBAAAAPo/haerin-newjeans.mp4",
 			components: [
 				{
 					type: 1,
